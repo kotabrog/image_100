8b702c08acff7b3bd4afc1000f1ce5611dac5709
kota <suzuki.kota0331@gmail.com>
2020-09-23 14:31:37 +0900

    add sobel_filter

--- image_100/filtering.py
+++ image_100/filtering.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .gray import rgb_to_gray
 
 # reduce colors
 
@@ -106,3 +107,126 @@ def mean_filter(img, kernel=(3, 3)):
 							for j in range(img.shape[1])]
 							for i in range(img.shape[0])])
 	return np.round(pad_img).astype('uint8')
+
+# Motion filter
+
+def motion_filter(img, kernel=(3, 3)):
+	half_kernel_h = kernel[0] // 2
+	half_kernel_w = kernel[1] // 2
+	half_under_h = (kernel[0] - 1) // 2
+	half_right_w = (kernel[1] - 1) // 2
+	size = min(kernel[0], kernel[1])
+	pad_img = np.pad(img, [(half_kernel_h, half_under_h), 
+							(half_kernel_w, half_right_w), (0, 0)], 'reflect') 
+	pad_img = np.array([[[np.mean([pad_img[i + l, j + l, k] for l in range(size)])
+							for k in range(3)]
+							for j in range(img.shape[1])]
+							for i in range(img.shape[0])])
+	return np.round(pad_img).astype('uint8')
+
+# Max-min filter
+
+def max_min_filter(img, kernel=(3, 3)):
+	if len(img.shape) == 3:
+		_img = rgb_to_gray(img)
+	else:
+		_img = img
+	half_kernel_h = kernel[0] // 2
+	half_kernel_w = kernel[1] // 2
+	half_under_h = (kernel[0] - 1) // 2
+	half_right_w = (kernel[1] - 1) // 2
+	pad_img = np.pad(_img, [(half_kernel_h, half_under_h), 
+							(half_kernel_w, half_right_w)], 'edge') 
+	pad_img = np.array([[np.max(pad_img[i: i + kernel[0], 
+										j: j + kernel[1]])
+							- np.min(pad_img[i: i + kernel[0], 
+										j: j + kernel[1]])
+							for j in range(img.shape[1])]
+							for i in range(img.shape[0])])
+	return np.round(pad_img).astype('uint8')
+
+# Differential filter
+
+def differential_filter(img, mode='y'):
+	if len(img.shape) == 3:
+		_img = rgb_to_gray(img)
+	else:
+		_img = img
+	if mode == 'y':
+		dif_img = np.pad(_img, [(1, 0), (0, 0)], 'reflect').astype(int)
+		return np.abs(dif_img[:-1, :] - _img).astype('uint8')
+	else:
+		dif_img = np.pad(_img, [(0, 0), (1, 0)], 'reflect').astype(int)
+		return np.abs(dif_img[:, :-1] - _img).astype('uint8')
+
+# Prewitt filter
+
+def prewitt_filter(img, k_size=(3, 3), mode='y'):
+	if len(img.shape) == 3:
+		_img = rgb_to_gray(img)
+	else:
+		_img = img
+	p_filter = np.zeros(k_size)
+	if mode == 'y':
+		if k_size[0] == 1:
+			return None
+		p_filter[0] = 1
+		p_filter[-1] = -1
+	else:
+		if k_size[1] == 1:
+			return None
+		p_filter[:, 0] = 1
+		p_filter[:, -1] = -1
+	half_k_size_h = k_size[0] // 2
+	half_k_size_w = k_size[1] // 2
+	half_under_h = (k_size[0] - 1) // 2
+	half_right_w = (k_size[1] - 1) // 2
+	pad_img = np.pad(_img, [(half_k_size_h, half_under_h), 
+							(half_k_size_w, half_right_w)], 'reflect')
+	pad_img = np.array([[np.abs(np.mean(p_filter * 
+							pad_img[i: i + k_size[0], j: j + k_size[1]]))
+							for j in range(img.shape[1])]
+							for i in range(img.shape[0])])
+	return np.round(pad_img).astype('uint8')
+
+# sobel filter
+
+def sobel_filter(img, k_size=(3, 3), mode='y'):
+	if len(img.shape) == 3:
+		_img = rgb_to_gray(img)
+	else:
+		_img = img
+	half_k_size_h = k_size[0] // 2
+	half_k_size_w = k_size[1] // 2
+	half_under_h = (k_size[0] - 1) // 2
+	half_right_w = (k_size[1] - 1) // 2
+	s_filter = np.zeros(k_size)
+	if mode == 'y':
+		if k_size[0] == 1:
+			return None
+		s_filter[0] = 1
+		s_filter[-1] = -1
+		s_filter[0, half_k_size_w] = 2
+		s_filter[-1, half_k_size_w] = -2
+		if k_size[0] % 2 == 0:
+			s_filter[0, half_k_size_w - 1] = 2
+			s_filter[-1, half_k_size_w - 1] = -2
+	else:
+		if k_size[1] == 1:
+			return None
+		s_filter[:, 0] = 1
+		s_filter[:, -1] = -1
+		s_filter[half_k_size_h, 0] = 2
+		s_filter[half_k_size_h, 1] = -2
+		if k_size[0] % 2 == 0:
+			s_filter[half_k_size_h - 1, 0] = 2
+			s_filter[half_k_size_h - 1, -1] = -2
+	pad_img = np.pad(_img, [(half_k_size_h, half_under_h), 
+							(half_k_size_w, half_right_w)], 'reflect')
+	pad_img = np.array([[np.sum(s_filter * 
+							pad_img[i: i + k_size[0], j: j + k_size[1]])
+							for j in range(img.shape[1])]
+							for i in range(img.shape[0])])
+	pad_img -= pad_img.min()
+	pad_img /= pad_img.max()
+	return pad_img
